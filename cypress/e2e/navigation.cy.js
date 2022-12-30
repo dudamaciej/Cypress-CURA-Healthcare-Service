4cadbc4d1922a4165829f5c99f6f61bb21227e50
dudamaciej <maciekduda99@gmail.com>
2022-12-30 15:48:28 +0100

    fix typo

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,30 +1,30 @@
 describe('Navigation', () => {
-   
+
     beforeEach(() => {
         cy.visit('/');
     })
 
     it('verifies navigation toggle', () => {
         cy.get('#menu-toggle').click()
-        cy.get('#sidebar-wrapper').should('have.class','active')
+        cy.get('#sidebar-wrapper').should('have.class', 'active')
         cy.get('#menu-toggle').click()
-        cy.get('#sidebar-wrapper').should('not.have.class','active')
+        cy.get('#sidebar-wrapper').should('not.have.class', 'active')
     })
 
-    it('verifies navigation without log in', () => {
+    it('verifies navigation without logging', () => {
         cy.fixture('nav_options').then((menuOptions) => {
             cy.verifyNavigationOptions(menuOptions.anonymusUserOptions)
-		})
+        })
     })
 
-    it('verifies navigation after log in', () => {
+    it('verifies navigation after logging', () => {
         cy.visit('/profile.php')
         cy.fixture('demo_account').then((user) => {
-            cy.login(user.username,user.password)
+            cy.login(user.username, user.password)
         })
         cy.fixture('nav_options').then((menuOptions) => {
             cy.verifyNavigationOptions(menuOptions.loggedUserOptions)
-		})
-       
+        })
+
     })
 })
